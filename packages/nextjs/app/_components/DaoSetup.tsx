a545914aec3417a1af91c02b56641796e5de6d08
nightmarefox12 <carloshenriquez392004@gmail.com>
2025-12-09 17:20:20 -0400

    feat: Add DAO setup component and page for initial address management and navigation.

---
+++ packages/nextjs/app/_components/DaoSetup.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useEffect, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
+import { LOCAL_STORAGE_KEYS } from "~~/constants/localStorage";
+import { useDaoStore } from "~~/services/store/dao.store";
+import { useHeaderStore } from "~~/services/store/header.store";
+
+export const DaoSetup: React.FC = () => {
+  const { setShowHeader } = useHeaderStore();
+  const { daoAddress, setDaoAddress } = useDaoStore();
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  //effects
+  useEffect(() => {
+    const daoStoraged = localStorage.getItem(LOCAL_STORAGE_KEYS.DAO_ADDRESS);
+
+    if (daoStoraged === null) {
+      setShowHeader(false);
+      startTransition(() => {
+        router.push("/daos");
+      });
+    } else {
+      if (daoAddress === "") setDaoAddress(daoStoraged!);
+    }
+    setShowHeader(true);
+  }, [router, setShowHeader, setDaoAddress, daoAddress]);
+
+  if (isPending) {
+    return (
+      <main className="min-h-screen flex items-center justify-center">
+        <Loader className="w-10 h-10 animate-spin" />
+      </main>
+    );
+  }
+};
--- packages/nextjs/app/dao/page.tsx
+++ packages/nextjs/app/dao/page.tsx
@@ -1,58 +1,14 @@
 "use client";
 
-import { useEffect, useTransition } from "react";
-import { Loader } from "lucide-react";
+import { DaoSetup } from "../_components/DaoSetup";
 import type { NextPage } from "next";
-import { useRouter } from "next-nprogress-bar";
-import { useAccount } from "wagmi";
-import { LOCAL_STORAGE_KEYS } from "~~/constants/localStorage";
-import { useScaffoldReadContract } from "~~/hooks/scaffold-eth";
-import { useDaoStore } from "~~/services/store/dao.store";
-import { useHeaderStore } from "~~/services/store/header.store";
 
 const Home: NextPage = () => {
-  const { address: userAddress } = useAccount();
-  const { setShowHeader } = useHeaderStore();
-  const { daoAddress, setDaoAddress } = useDaoStore();
-  const router = useRouter();
-
-  const [isPending, startTransition] = useTransition();
-
-  //smart contract
-  const { data: DEFAULT_ADMIN_ROLE } = useScaffoldReadContract({
-    contractName: "AgoraDao",
-    functionName: "DEFAULT_ADMIN_ROLE",
-    contractAddress: daoAddress,
-  });
-  const { data: isOwner } = useScaffoldReadContract({
-    contractName: "AgoraDao",
-    functionName: "isRole",
-    contractAddress: daoAddress,
-    args: [DEFAULT_ADMIN_ROLE, userAddress],
-  });
-
-  //effects
-  useEffect(() => {
-    if (localStorage.getItem(LOCAL_STORAGE_KEYS.DAO_ADDRESS) === null) {
-      setShowHeader(false);
-      startTransition(() => {
-        router.push("/daos");
-      });
-    } else {
-      setDaoAddress(localStorage.getItem(LOCAL_STORAGE_KEYS.DAO_ADDRESS)!);
-    }
-    setShowHeader(true);
-  }, [router, setShowHeader, setDaoAddress]);
-
-  if (isPending) {
-    return (
-      <main className="min-h-screen flex items-center justify-center">
-        <Loader className="w-10 h-10 animate-spin" />
-      </main>
-    );
-  }
-
-  return <main>{isOwner?.toString()}</main>;
+  return (
+    <main>
+      <DaoSetup />
+    </main>
+  );
 };
 
 export default Home;
