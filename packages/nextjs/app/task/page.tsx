3c2862e9840a4c3cc30a18cff77093e67c0eb06a
nightmarefox12 <carloshenriquez392004@gmail.com>
2025-12-15 09:57:39 -0400

    feat: add new TaskPage component rendering DaoSetup.

--- packages/nextjs/app/task/page.tsx
+++ packages/nextjs/app/task/page.tsx
@@ -1,9 +1,14 @@
 "use server";
 
+import { DaoSetup } from "../_components/DaoSetup";
 import { NextPage } from "next";
 
 const TaskPage: NextPage = () => {
-  return <div>TaskPage</div>;
+  return (
+    <main>
+      <DaoSetup />
+    </main>
+  );
 };
 
 export default TaskPage;
